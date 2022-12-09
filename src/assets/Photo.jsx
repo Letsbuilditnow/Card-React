4614168967845b595fec4e5249c3a5e8d69b5d80
mmmmm1233212344 <abhaymilan8@gmail.com>
2022-12-09 23:50:46 +0530

    Component Created

---
+++ src/assets/Photo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './photo.css'
+function photo() {
+  return (
+    <div>
+        <img src="../../public/WFXV1417.JPG" alt="photo-image" />
+    </div>
+  )
+}
+
+export default photo
